7f891e0fd1dd1f2fd7896d63095ff92bfdf3407e
strick <strick@yak.net>
2023-05-21 17:52:06 -0700

    uupdate config & fetch

--- config.sh
+++ config.sh
@@ -1,11 +1,16 @@
-export COCO_CACHE_DIR="${COCO_CACHE_DIR:-$HOME/coco-cache}"
+COCO_MIRROR_DIR="${COCO_MIRROR_DIR:-$HOME/mirror}"
 
-export COCO_LWTOOLS_VERSION=lwtools-4.21
-export COCO_CMOC_VERSION=cmoc-0.1.82
-export COCO_GCCRETRO_VERSION=gcc-4.6.4
+COCO_LWTOOLS_VERSION=lwtools-4.21
+COCO_CMOC_VERSION=cmoc-0.1.82
+COCO_GCCRETRO_VERSION=gcc-4.6.4
+
 COCO_TOOLSHED_COMMIT=aea8b5aa290522af9785ab33d2e098a8b870cfdb
 COCO_NITROS9_COMMIT=6f29ea34f8bbf549bbe31be7d7a92c70fbfe3df8
 COCO_FROBIO_COMMIT=03a1b44e65e4bbc37c8ab347b122baf927690544
+
+COCO_TOOLSHED_REPO=https://github.com/n6il/toolshed
+COCO_NITROS9_REPO=https://github.com/n6il/nitros9
+COCO_FROBIO_REPO=https://github.com/strickyak/frobio
 
 COCO_LWTOOLS_URL="http://www.lwtools.ca/releases/lwtools/$COCO_LWTOOLS_VERSION.tar.gz"
 COCO_CMOC_URL="http://perso.b2b2c.ca/~sarrazip/dev/$COCO_CMOC_VERSION.tar.gz"
--- fetch.sh
+++ fetch.sh
@@ -7,43 +7,56 @@ export SHELF_TIMESTAMP=$(date +T%s)
 cd $SHELF
 source ./config.sh
 
-# Fetch tarballs into cache dir.
-mkdir -p "$COCO_CACHE_DIR"
+# Tarballs Mirrors.
+mkdir -p "$COCO_MIRROR"
 
-test -s "$COCO_CACHE_DIR/$COCO_LWTOOLS_VERSION.tar.gz" ||
-  ( cd "$COCO_CACHE_DIR" &&
+test -s "$COCO_MIRROR/$COCO_LWTOOLS_VERSION.tar.gz" ||
+  ( cd "$COCO_MIRROR" &&
     wget "$COCO_LWTOOLS_URL" )
 
-test -s "$COCO_CACHE_DIR/$COCO_CMOC_VERSION.tar.gz" ||
-  ( cd "$COCO_CACHE_DIR" &&
+test -s "$COCO_MIRROR/$COCO_CMOC_VERSION.tar.gz" ||
+  ( cd "$COCO_MIRROR" &&
     wget "$COCO_CMOC_URL" )
 
-test -s "$COCO_CACHE_DIR/$COCO_GCCRETRO_VERSION.tar.bz2" ||
-  ( cd "$COCO_CACHE_DIR" &&
+test -s "$COCO_MIRROR/$COCO_GCCRETRO_VERSION.tar.bz2" ||
+  ( cd "$COCO_MIRROR" &&
     wget "$COCO_GCCRETRO_URL" )
 
 # Unbundle the tarballs.
 test -d "$COCO_LWTOOLS_VERSION" ||
-  tar xzf "$COCO_CACHE_DIR/$COCO_LWTOOLS_VERSION.tar.gz"
+  tar xzf "$COCO_MIRROR/$COCO_LWTOOLS_VERSION.tar.gz"
 
 test -d "$COCO_CMOC_VERSION" ||
-  tar xzf "$COCO_CACHE_DIR/$COCO_CMOC_VERSION.tar.gz"
+  tar xzf "$COCO_MIRROR/$COCO_CMOC_VERSION.tar.gz"
 
 test -d "$COCO_GCCRETRO_VERSION" ||
-  tar xjf "$COCO_CACHE_DIR/$COCO_GCCRETRO_VERSION.tar.bz2"
+  tar xjf "$COCO_MIRROR/$COCO_GCCRETRO_VERSION.tar.bz2"
+
+# Github Mirrors.
+test -d "$COCO_MIRROR/toolshed" ||
+  ( cd "$COCO_MIRROR" &&
+    git clone --mirror $COCO_TOOLSHED_REPO toolshed )
+
+test -d "$COCO_MIRROR/nitros9" ||
+  ( cd "$COCO_MIRROR" &&
+    git clone --mirror $COCO_NITROS9_REPO nitros9 )
+
+test -d "$COCO_MIRROR/frobio" ||
+  ( cd "$COCO_MIRROR" &&
+    git clone --mirror $COCO_FROBIO_REPO frobio )
 
 # Github.
 test -d toolshed || {
-  git clone https://github.com/n6il/toolshed toolshed &&
+  git clone "$COCO_MIRROR/toolshed" toolshed &&
   ( cd toolshed ; git checkout -b $SHELF_TIMESTAMP $COCO_TOOLSHED_COMMIT )
 }
 
 test -d nitros9 || {
-  git clone https://github.com/n6il/nitros9 &&
+  git clone "$COCO_MIRROR/nitros9" &&
   ( cd nitros9 ; git checkout -b $SHELF_TIMESTAMP $COCO_NITROS9_COMMIT )
 }
 
 test -d frobio || {
-  git clone https://github.com/strickyak/frobio.git &&
+  git clone "$COCO_MIRROR/frobio" &&
   ( cd frobio ; git checkout -b $SHELF_TIMESTAMP $COCO_FROBIO_COMMIT )
 }
