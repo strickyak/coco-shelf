7567d2eaa25ecbf108eb64e1862484c85de0697c
Henry Strickland <strick@yak.net>
2024-01-28 23:53:12 -0800

    slot scripts/lwasm-with-listing.sh

--- scripts/lwasm-with-listing.sh
+++ scripts/lwasm-with-listing.sh
@@ -1,24 +1,38 @@
 #!/bin/sh
-set -eu
+set -x
+export PATH=$(dirname $0):/usr/bin:/bin
+
+XLIST='--list-nofiles'
+XLIST=''
 
 FOR_ASM=
 FOR_O=
+NEXT_O=
 
 for x
 do
-	case "$x" in
-	*.asm )
-		set -x
-		FOR_ASM="--map=$(basename $x .asm).map --list=$(basename $x .asm).list --list-nofiles"
-		set +x
-		;;
-	-o* )
-		set -x
-		y=$(expr match "$x" '..\(.*\)')
-		FOR_O="--map=$(basename $y).map --list=$(basename $y).list --list-nofiles"
-		set +x
-		;;
-	esac
+        case "@$NEXT_O@$x" in
+        @1@* )
+        	set -x ; #echo {1} >&2
+        	FOR_O="--map=$(basename $x).map --list=$(basename $x).list $XLIST"
+        	NEXT_O=
+        	;;
+
+        @@*.asm )
+        	set -x ; #echo {2} >&2
+        	FOR_ASM="--map=$(basename $x .asm).map --list=$(basename $x .asm).list $XLIST"
+        	;;
+        @@-o )
+        	set -x ; #echo {3} >&2
+        	NEXT_O=1
+        	;;
+        	
+        @@-o* )
+        	set -x ; #echo {4} >&2
+        	y=$(expr match "$x" '..\(.*\)')
+        	FOR_O="--map=$(basename $y).map --list=$(basename $y).list $XLIST"
+        	;;
+        esac
 done
 
 set -eux
@@ -26,11 +40,11 @@ set -eux
 # If $FOR_O is non-empty, it takes precedence.
 # Else, fall back to $FOR_ASM.
 case $FOR_O in
-	"" )
-		exec lwasm.org "$@" $FOR_ASM
-		;;
+        "" )
+        	exec lwasm.orig "$@" $FOR_ASM
+        	;;
 
-	* )
-		exec lwasm.orig "$@" $FOR_O
-		;;
+        * )
+        	exec lwasm.orig "$@" $FOR_O
+        	;;
 esac
