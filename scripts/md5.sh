1909ff0a391ea515ddd306fee3cce0ad841f6d2b
Henry Strickland <strick@yak.net>
2024-01-28 23:58:02 -0800

    md5 script.  usually now it calls md5sum.

---
+++ scripts/md5.sh
@@ -0,0 +1,10 @@
+for cmd in /usr/bin/md5sum /bin/md5sum /usr/bin/md5 /bin/md5
+do
+  if test -x "$cmd"
+  then
+	set -x ; exec "$cmd" "$@"
+  fi
+done
+
+echo "ERROR: did not find md5sum nor md5" >&2
+exit 13
