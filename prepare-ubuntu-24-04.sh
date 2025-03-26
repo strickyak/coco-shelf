b9a7d5ab2823e2dca881e13c1c8fea8ae0877c57
Henry Strickland <strick@yak.net>
2025-03-25 19:10:22 -0700

    add prepare-ubuntu-24-04.sh

---
+++ prepare-ubuntu-24-04.sh
@@ -0,0 +1,20 @@
+# Execute this script FIRST
+# to install & update needed packages
+# on Ubuntu 24.04 Noble (including LTS),
+# before you use `make` in `coco-shelf/` the first time.
+#
+# Notice this first updates all your packages.
+# You could leave out the first two commands,
+# but that's generally a bad idea.
+
+set -ex
+
+sudo DEBIAN_FRONTEND=noninteractive  apt -y update < /dev/null
+sudo DEBIAN_FRONTEND=noninteractive  apt -y upgrade < /dev/null
+sudo DEBIAN_FRONTEND=noninteractive  apt -y install gcc make flex bison gdb build-essential
+sudo DEBIAN_FRONTEND=noninteractive  apt -y install git golang zip curl python3-serial
+sudo DEBIAN_FRONTEND=noninteractive  apt -y install libgmp-dev libmpfr-dev libmpc-dev libfuse-dev
+# for Pi Pico:
+sudo DEBIAN_FRONTEND=noninteractive  apt -y install cmake gcc-arm-none-eabi
+
+echo OKAY.
