b52bf681d279fb0fd8431349ef05335861330e0b
Henry Strickland <strick@yak.net>
2025-04-11 14:24:19 -0700

    nit "..." in scripts/go-install-static.sh

--- scripts/go-install-static.sh
+++ scripts/go-install-static.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
 
-SHELF=$( cd $(dirname $(dirname $0)) && pwd )
+SHELF="$( cd $(dirname $(dirname $0)) && pwd )"
 
 set -eux
 GOBIN="$SHELF/bin" CGO_ENABLED=0 go install -x -ldflags '-extldflags "-static"' "$@"
