51677d50be52371f4f7c66707414d2e52daa71f7
Henry Strickland <strick@yak.net>
2026-04-14 01:05:13 -0400

    add to prepare: tcl expect xxd

--- prepare-ubuntu-24-04.sh
+++ prepare-ubuntu-24-04.sh
@@ -12,7 +12,7 @@ set -ex
 sudo DEBIAN_FRONTEND=noninteractive  apt -y update < /dev/null
 sudo DEBIAN_FRONTEND=noninteractive  apt -y upgrade < /dev/null
 sudo DEBIAN_FRONTEND=noninteractive  apt -y install gcc make flex bison gdb build-essential
-sudo DEBIAN_FRONTEND=noninteractive  apt -y install git golang zip curl python3-serial
+sudo DEBIAN_FRONTEND=noninteractive  apt -y install git golang zip curl python3-serial tcl expect xxd
 sudo DEBIAN_FRONTEND=noninteractive  apt -y install libgmp-dev libmpfr-dev libmpc-dev libfuse-dev
 sudo DEBIAN_FRONTEND=noninteractive  apt -y install cmake gcc-arm-none-eabi libusb-1.0-0-dev pkg-config
 
